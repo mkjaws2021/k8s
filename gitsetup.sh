f0a8ed980b3b1d233cf1633ba070ff3276f7e839
mkjaws2021 <mkj.aws2021@gmail.com>
2021-04-19 01:59:02 +0000

    Added gitsetup.sh for alias and config

---
+++ gitsetup.sh
@@ -0,0 +1,13 @@
+#bin/bash
+git config --global user.name "mkjaws2021"
+git config --global user.email "mkj.aws2021@gmail.com"
+
+echo alias kga=\'kubectl get all -o wide\' >> ~/.bashrc 
+echo alias kgd=\'kubectl get deployments -o wide\'>> ~/.bashrc
+echo alias kgp=\'kubectl get pods -o wide\'>> ~/.bashrc
+echo alias kgn=\'kubectl get namespace\'>> ~/.bashrc
+echo alias kgr=\'kubectl get replicaset -o wide\'>> ~/.bashrc
+#source ~/.bashrc
+#source ~/.bashrc
+alias brc='source ~/.bashrc'
+cd ~/k8s/
