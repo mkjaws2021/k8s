be1305f00918c24871a396c1be693120a91401cc
mkjaws2021 <76777762+mkjaws2021@users.noreply.github.com>
2021-09-09 09:06:37 +0530

    Update and rename K8bashcompletion_v1.sh to k8s_bashcompletion.sh

---
+++ k8s_bashcompletion.sh
@@ -0,0 +1,38 @@
+"#!/bin/bash
+get_distribution() {
+        lsb_dist=""
+        # Every system that we officially support has /etc/os-release
+        if [ -r /etc/os-release ]; then
+                lsb_dist="$(. /etc/os-release && echo "$ID")"
+        fi
+        # Returning an empty string here should be alright since the
+        # case statements don't act unless you provide an actual value
+        echo "$lsb_dist"
+}
+
+lsb_dist=$( get_distribution )
+lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
+echo $lsb_dist
+
+case "$lsb_dist" in
+  ubuntu) 
+           apt-get install bash-completion -y 
+           /bin/bash -c 'source /usr/share/bash-completion/bash_completion'
+           /bin/bash -c 'source <(kubectl completion bash)' >>~/.bashrc
+           echo 'alias k=kubectl' >>~/.bashrc
+           echo 'alias kgp=\"kubectl get pods -o wide --show-labels\"' >>~/.bashrc
+           echo 'alias kgd=\"kubectl get deployments -o wide --show-labels\"' >>~/.bashrc
+           echo 'complete -F __start_kubectl k' >>~/.bashrc
+           /bin/bash -c 'source ~/.bashrc'
+  ;;
+  centos) echo " sudo yum install bash-completion -y  
+             /bin/bash -c 'source /usr/share/bash-completion/bash_completion'
+           /bin/bash -c 'source <(kubectl completion bash)' >>~/.bashrc
+           echo 'alias k=kubectl' >>~/.bashrc
+           echo 'alias kgp=\"kubectl get pods -o wide --show-labels\"' >>~/.bashrc
+           echo 'alias kgd=\"kubectl get deployments -o wide --show-labels\"' >>~/.bashrc
+           echo 'complete -F __start_kubectl k' >>~/.bashrc
+           /bin/bash -c 'source ~/.bashrc'
+  ;;
+  
+esac 
