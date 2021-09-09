aac518ee98610f71fa5fc9569d108b95c0047752
mkjaws2021 <76777762+mkjaws2021@users.noreply.github.com>
2021-09-09 11:28:48 +0530

    Update k8s_bashcompletion.sh

--- k8s_bashcompletion.sh
+++ k8s_bashcompletion.sh
@@ -1,4 +1,3 @@
-"#!/bin/bash
 get_distribution() {
         lsb_dist=""
         # Every system that we officially support has /etc/os-release
@@ -15,24 +14,22 @@ lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 echo $lsb_dist
 
 case "$lsb_dist" in
-  ubuntu) 
-           apt-get install bash-completion -y 
+  ubuntu)
+           apt-get install bash-completion -y
            /bin/bash -c 'source /usr/share/bash-completion/bash_completion'
            /bin/bash -c 'source <(kubectl completion bash)' >>~/.bashrc
            echo 'alias k=kubectl' >>~/.bashrc
-           echo 'alias kgp=\"kubectl get pods -o wide --show-labels\"' >>~/.bashrc
-           echo 'alias kgd=\"kubectl get deployments -o wide --show-labels\"' >>~/.bashrc
+           echo "alias kgp='kubectl get pods -o wide --show-labels'" >>~/.bashrc
+           echo "alias kgd=\'kubectl get deployments -o wide --show-labels\'" >>~/.bashrc
            echo 'complete -F __start_kubectl k' >>~/.bashrc
            /bin/bash -c 'source ~/.bashrc'
   ;;
-  centos)  sudo yum install bash-completion -y  
+  centos)  sudo yum install bash-completion -y
            /bin/bash -c 'source /usr/share/bash-completion/bash_completion'
            /bin/bash -c 'source <(kubectl completion bash)' >>~/.bashrc
            echo 'alias k=kubectl' >>~/.bashrc
-           echo 'alias kgp=\"kubectl get pods -o wide --show-labels\"' >>~/.bashrc
-           echo 'alias kgd=\"kubectl get deployments -o wide --show-labels\"' >>~/.bashrc
+           echo "alias kgp='kubectl get pods -o wide --show-labels'" >>~/.bashrc
+           echo "alias kgd=\'kubectl get deployments -o wide --show-labels\'" >>~/.bashrc
            echo 'complete -F __start_kubectl k' >>~/.bashrc
            /bin/bash -c 'source ~/.bashrc'
   ;;
-  
-esac 
